2af18da96c1abcd13b9ceece24866e8b7e48080b
Clemente <clementesc@gmail.com>
2021-07-20 20:26:58 -0300

    Inclusão do Enum OrderStatus

--- src/main/java/com/example/ecommerce/entities/Order.java
+++ src/main/java/com/example/ecommerce/entities/Order.java
@@ -1,5 +1,6 @@
 package com.example.ecommerce.entities;
 
+import com.example.ecommerce.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -24,6 +25,9 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    // Mantemos um valor inteiro para nosso Enum, pois no banco de dados será um integer
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
@@ -31,10 +35,20 @@ public class Order implements Serializable {
     public Order() {
     }
 
-    public Order(Integer id, Instant moment, User client) {
+    public Order(Integer id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
+    }
+
+    public OrderStatus getOrderStatus(){
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus){
+        if (orderStatus != null)
+            this.orderStatus = orderStatus.getCode();
     }
 
     @Override
